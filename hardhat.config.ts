9a9faad52a2f65da6f7c6d203a19cc683c4ea5f6
Maxcoin Developer <maxcoindev@gmail.com>
2025-05-09 21:24:13 +0530

    add foundry

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,26 @@
 import type { HardhatUserConfig } from "hardhat/config";
-// import "@nomicfoundation/hardhat-toolbox-viem";
+import "@nomicfoundation/hardhat-foundry";
+// Replace toolbox 
+import "@nomicfoundation/hardhat-verify";
+import "@nomicfoundation/hardhat-viem";
+import "@nomicfoundation/hardhat-ignition-viem";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+import chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+import "@nomicfoundation/hardhat-network-helpers";
+
+chai.use(chaiAsPromised);
+
+// end replace toolbox
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  gasReporter: {
+    enabled: true,
+    currency: "USD"
+  }
 };
 
 export default config;
